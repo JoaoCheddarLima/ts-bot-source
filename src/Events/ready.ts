93f79866bb85f4197d5dce27d127e5be1eb921d7
JVRL18 <joaogamerpolado@gmail.com>
2023-05-02 23:06:28 -0300

    STYLE: format code && text

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -5,11 +5,11 @@ dotenv.config()
 export const event: Event = {
     name: 'ready',
     run: (client) => {
-        console.log(`[!]Bot Status: ONLINE as: [${client.user?.tag}]`)
+        console.log(`[!]Bot Status: ONLINE as [${client.user?.tag}]`)
         if (process.env.USE_DB?.toLowerCase() == 'yes') {
 
         } else {
-            console.log(`\n[!]No database mode enabled, local json will be used instead.\n`)
+            console.log(`\n[!]No database mode enabled, local json will be used instead.`)
         }
     }
 }
